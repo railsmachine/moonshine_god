b705e2d81612b20be7cdd08a23f4a3001d8eaea7
Rob Lingle <rob@actsasif.com>
2009-06-29 11:14:17 -0400

    use god:restart after deploy to ensure tasks are added/removed

--- recipes/god.rb
+++ recipes/god.rb
@@ -1,7 +1,18 @@
 namespace :god do
+  desc "Restart god"
+  task :restart do
+    sudo 'god quit' # upstart will restart
+  end
+
   desc "Reload god configuration."
   task :reload do
     sudo 'god load /etc/god/god.conf'
   end
+  
+  desc "Display status of god watches"
+  task :status do
+    sudo 'god status'
+  end
+
 end
-after 'deploy:restart', 'god:reload'
+after 'deploy', 'god:restart'
