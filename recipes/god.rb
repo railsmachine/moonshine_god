e49f533a3e7ceaada05e9d67fea626785578591b
Lee Jones <scribblethink@gmail.com>
2011-06-02 10:54:30 -0400

    Run god cap tasks on servers with the :god role
    Updating from a Previous Version:
    
    * Add the :god role to the server(s) in your capistrano deploy file(s) that are running god
    * See the README for an example

--- recipes/god.rb
+++ recipes/god.rb
@@ -3,19 +3,19 @@ namespace :god do
   Restart god to remove old watches. Would ideally be replaced with something
   like 'god reload' - http://github.com/eric/god/commits/god-reload
   DESC
-  task :restart do
+  task :restart, :roles => :god do
     sudo 'stop god || true'
     sudo 'start god'
     sleep 5
   end
 
   desc "Reload god configuration."
-  task :reload do
+  task :reload, :roles => :god do
     sudo 'god load /etc/god/god.conf'
   end
   
   desc "Display status of god watches"
-  task :status do
+  task :status, :roles => :god do
     sudo 'god status'
   end
 
